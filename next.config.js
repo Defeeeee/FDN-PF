e808266eb10dd98ed7a6ed3ce7716766acae258b
Federico Diaz Nemeth <fdiaznemeth@gmail.com>
2025-11-27 16:14:12 -0300

    Add remaining files

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+        allowedOrigins: ["fdiaznem.com.ar", "localhost:3003"]
+    }
+}
+
+export default nextConfig
